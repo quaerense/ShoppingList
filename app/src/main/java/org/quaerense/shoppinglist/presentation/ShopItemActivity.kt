fe0f9ff9ec42554db190fe42d7cf835a5ac678e9
Alexey Bakaykin <color.ui>
2022-01-22 19:21:40 +0300

    add shopItemViewModel

--- app/src/main/java/org/quaerense/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/org/quaerense/shoppinglist/presentation/ShopItemActivity.kt
@@ -2,11 +2,15 @@ package org.quaerense.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import org.quaerense.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+
     }
 }
